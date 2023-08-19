0d9b5d806e9c390490bf9df058447d25bd537672
Hongzip <hongzip.kim@mail.utoronto.ca>
2023-08-19 09:53:54 -0400

    Create Home & About Us page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/home.jsx";
+import About from "./routes/about.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path:"/about",
+        element: <About />
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,17 @@ export default {
         primary: 'Montserrat',
         title: 'Montserrat',
       },
+      colors: {
+        bg: '#0A61C2',
+        lightblue: '#DFF8FE',
+        primary: '#0177FB',
+        secondary: '#A9E5FF',
+        white: '#FFFFFF',
+        black: '#000000',
+        blue: '#0A61C2',
+        dark: '#1C52A3',
+      },
     },
   },
   plugins: [],
 }
-
