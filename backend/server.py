7cb1cf590c30a91ff7d5f246c0401edda0049113
panchk5 <122956248+panchk5@users.noreply.github.com>
2023-08-19 11:11:29 -0400

    Committed assets plus trained AI model
    Trained AI model using google colab and Roboflow datasets.

--- backend/server.py
+++ backend/server.py
@@ -7,3 +7,28 @@ import os
 
 app = Flask(__name__)
 CORS(app)
+
+upload_folder = './assets'
+
+from vision import image_ai
+@app.route('/upload-image', methods=['POST'])
+def handle_image_upload():
+    try:      
+        data = request.json
+        image_data = data['imageData']
+        decoded_data = base64.b64decode(image_data.split(',')[1])
+
+        image_name = f'uploaded_image_.jpg'
+        image_path = os.path.join(upload_folder, image_name)
+        with open(image_path, 'wb') as f:
+            f.write(decoded_data)
+
+        # returns as json
+        result = image_ai('./assets/test_image.jpg')
+        
+        return result
+        # return jsonify({'message': 'Image uploaded and processed successfully'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
