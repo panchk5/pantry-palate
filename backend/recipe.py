a053754defd058995c5590381949fa36db3b03fa
Jingyue Wu <75918217+Jingyue-Wu@users.noreply.github.com>
2023-08-19 20:22:31 -0400

    Optimize recipe API searching

--- backend/recipe.py
+++ backend/recipe.py
@@ -1,10 +1,34 @@
+# import os
+# import requests
+# key = os.getenv("SPOON_API_KEY")
+
+# API_URL = "https://api.spoonacular.com/recipes/complexSearch"
+
+# # food_list = ["beef", "tomato"]
+
+# def make_recipe(food_list):
+#     headers = {"Content-Type": "application/json"}
+#     params = {
+#         "ingredients": ",".join(food_list),
+#         "number": 1,
+#         "apiKey": key,
+#         "instructionsRequired": True,
+#         "addRecipeInformation": True,
+#     }
+
+#     response = requests.get(API_URL, headers=headers, params=params)
+#     recipeData = response.json()
+#     return recipeData
+
+
 import os
 import requests
 key = os.getenv("SPOON_API_KEY")
 
-API_URL = "https://api.spoonacular.com/recipes/complexSearch"
+API_URL = "https://api.spoonacular.com/recipes/findByIngredients"
 
 # food_list = ["beef", "tomato"]
+
 
 def make_recipe(food_list):
     headers = {"Content-Type": "application/json"}
@@ -14,11 +38,10 @@ def make_recipe(food_list):
         "apiKey": key,
         "instructionsRequired": True,
         "addRecipeInformation": True,
+        "ranking": 1,
+        "sort": "min-missing-ingredients",
     }
 
     response = requests.get(API_URL, headers=headers, params=params)
     recipeData = response.json()
     return recipeData
-
-# print(make_recipe(food_list))
-# Need to return to frontend
