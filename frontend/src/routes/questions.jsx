b49917d4e40072d92c2480331652488ac1eed0dd
Jingyue Wu <75918217+Jingyue-Wu@users.noreply.github.com>
2023-08-26 21:58:57 -0400

    Fix stuff

--- frontend/src/routes/questions.jsx
+++ frontend/src/routes/questions.jsx
@@ -1,36 +1,36 @@
-import { useContext, useEffect, useState } from 'react'
-import { Data, RecipeData } from '../components/context'
+import { useContext, useEffect, useState } from 'react';
+import { Data, RecipeData } from '../components/context';
 import { useNavigate } from 'react-router-dom';
 
 export default function Questions() {
   // const [ingredientList, setIngredientList] = useState([])
-  const [inputText, setInputText] = useState('')
-  const { modelData, setModelData } = useContext(Data)
+  const [inputText, setInputText] = useState('');
+  const { modelData, setModelData } = useContext(Data);
   const { recipeData, setRecipeData } = useContext(RecipeData);
   const navigate = useNavigate();
 
 
 
   useEffect(() => {
-    console.log(modelData)
-  }, [])
+    console.log(modelData);
+  }, []);
 
   const handleInputChange = (event) => {
-    setInputText(event.target.value)
-  }
+    setInputText(event.target.value);
+  };
 
   const handleFormSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (inputText.trim() !== '') {
       // check if already in list
       if (modelData.some((item) => item.name === inputText)) {
-        alert('already in list')
+        alert('already in list');
       } else {
-        setModelData([...modelData, { name: inputText }])
-        setInputText('')
+        setModelData([...modelData, { name: inputText }]);
+        setInputText('');
       }
     }
-  }
+  };
 
   const sendData = async () => {
     try {
@@ -39,7 +39,7 @@ export default function Questions() {
         headers: {
           'Content-Type': 'application/json', // Set the correct content type
         },
-        body: JSON.stringify({ recipeData: modelData}),
+        body: JSON.stringify({ recipeData: modelData }),
       });
 
       if (response.ok) {
@@ -51,11 +51,11 @@ export default function Questions() {
 
       const _data = await response.json();
 
-      setRecipeData(_data)
+      setRecipeData(_data);
 
       console.log(_data);
 
-      navigate('/recipes')
+      navigate('/recipes');
 
 
       // redirect to questions page
@@ -69,59 +69,70 @@ export default function Questions() {
   return (
     <>
       <div className='lg:m-auto lg:max-w-[40%] m-[10%]'>
-      <h1 className="text-2xl font-medium text-white mb-4">Ingredients 🧂</h1>
+        <h1 className="text-2xl font-medium text-white mb-4">Ingredients 🧂</h1>
         <br />
         <div className='bg-card rounded-3xl flex flex-row justify-start'>
-          
-<ul className="bg-white p-5 w-full rounded-2xl">
-  {modelData.map((item, index) => {
-    const itemNameWithoutUnderscores = item.name.replace(/_/g, ' ');
 
-    return (
-      <li key={index} className='flex gap-4 justify-between m-3'>
+          <ul className="bg-white p-5 w-full rounded-3xl">
+            {modelData.map((item, index) => {
+              const itemNameWithoutUnderscores = item.name.replace(/_/g, ' ');
 
-        <div className='flex'>
+              return (
+                <li key={index} className='flex gap-4 justify-between m-3'>
 
-        • {itemNameWithoutUnderscores}
+                  <div className='flex'>
 
-        </div>
+                    • {itemNameWithoutUnderscores}
 
-        <button
-          onClick={() => {
-            setModelData(modelData.filter((item, i) => i !== index))
-          }}
-          className='hover:bg-red-500 rounded-full transition-all hover:text-white flex flex-row justify-right'
-        >
-          ❌
-        </button>
-      </li>
-    );
-  })}
-</ul>
+                  </div>
+
+                  <button
+                    onClick={() => {
+                      setModelData(modelData.filter((item, i) => i !== index));
+                    }}
+                    className='hover:bg-red-500 rounded-full transition-all hover:text-white flex flex-row justify-right'
+                  >
+                    ❌
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </div>
         {/* Input text field that adds to ingredientList */}
         <form onSubmit={handleFormSubmit}>
           <label className='flex justify-center mt-3'>
             <input
               placeholder='Any missing ingredients?'
-              className='bg-card rounded-3xl text-sm p-3 m-2 w-full mb-8'
+              className='bg-card rounded-3xl text-sm p-3 my-2 w-full mb-8'
               type='text'
               value={inputText}
               onChange={handleInputChange}
             />
           </label>
-          <button
-            className='bg-primary rounded-3xl py-2 px-4 hover:bg-white transition-all outline-2 flex flex-row justify-center mb-8'
-            type='submit'
-          >
-            Add to List
-          </button>
+
+          <div className='flex justify-between'>
+            <button
+              className='bg-bg rounded-3xl text-white py-2 px-4 hover:bg-white hover:text-bg transition-all border-2 flex flex-row justify-center mb-8'
+              type='submit'
+            >
+              Add to List
+            </button>
+
+            <button
+              className='bg-white rounded-3xl text-bg py-2 px-4 transition-all border-2 flex flex-row justify-center mb-8'
+              onClick={sendData}
+            >
+              Confirm →
+            </button>
+          </div>
+
+          <a className='bottom-20 left-10 bg-dark text-white rounded-full py-2 px-4' href="/scan">&lt;</a>
+
         </form>
       </div>
 
-      <button className='px-4 py-2 bg-white text-bg text-sm rounded-full cursor-pointer' onClick={sendData}>
-          Confirm →
-      </button>
+
     </>
-  )
+  );
 }
